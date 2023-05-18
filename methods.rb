4fb5b8e5b89d39a73a95c29b15dbcfa10b45d13e
ringa-nyamvula <nyamvularinga@gmail.com>
2023-05-18 10:13:15 +0300

    Complete assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
-# Your code here!
+def greet_programmer()
+    puts "Hello, programmer!"
+   end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet("Naureen")
+
+
+def add(num1, num2)
+  return  num1 + num2
+end
+puts add(2,5)
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+puts greet_with_default()
+
+def halve(number)
+    if number % 2 == 0
+        return number / 2
+    else
+        return nil
+    end
+end
+puts halve(6)
